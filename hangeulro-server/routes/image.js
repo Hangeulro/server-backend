943fee3fc10a23c747c37211ad818dc693b902d8
iwin2471 <iwin12372@gmail.com>
2016-11-09 21:53:50 +0900

    modify user edit (enable to change image and name both)

--- hangeulro-server/routes/image.js
+++ hangeulro-server/routes/image.js
@@ -6,4 +6,8 @@ router.get('/:image', function(req, res, next) {
 });
 
 
+router.get('/users/:image', function(req, res, next) {
+    res.sendFile("/node/server-backend/hangeulro-server/upload/users/"+req.params.image+".png");
+});
+
 module.exports = router;
