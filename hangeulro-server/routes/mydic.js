c96f0c0beb6271052900f31bede261c575bdc207
iwin2471 <iwin12372@gmail.com>
2017-01-26 05:09:22 +0900

    [rest api] mydic update

--- hangeulro-server/routes/mydic.js
+++ hangeulro-server/routes/mydic.js
@@ -1,73 +1,67 @@
-var express = require('express');
-var router = express.Router();
+module.exports = (router, func)=>{
 
-router.post('/make', function(req, res, next){
-    var dicname = req.body.dicname+"";
-    var sub = req.body.sub+""
-    var token = req.body.token+"";
+  router.post('/make', (req, res, next)=>{
+    var params = ['dicname', 'sub', 'token'];
+    func.check_param(req.body, params, res);
 
-     Mydics.findOne({owner: token, dicname: dicname}, function(err, result){
-       if(err) return res.status(409).send("DB ERROR");
-       console.log(result)
-       if(result !== null){
-          return res.status(409).send("already exists");
-       }else{
-         var current = new Mydics({
-           owner: token,
-           dicname: dicname,
-           sub: sub,
-           favorite: []
-         });
-
-          current.save(function(err, data) {
-            if (err) return res.status(409).send("DB error");
-             Mydics.find({owner: token}, function(err, mydic){
-               if(err) res.send(409, "dberror");
-               res.status(200).send(mydic);
-            });
-         });
-
-      }
-    });
-
-});
-
-
-router.post('/add', function(req, res, next){
-   var dicname = req.body.dicname;
-   var token = req.body.token;
-   var word = req.body.word;
-   var check = 0;
-   
-   console.log(word);
-
+    var dicname = req.body.dicname;
+    var sub = req.body.sub;
+    var token = req.body.token;
 
     Mydics.findOne({owner: token, dicname: dicname}, function(err, result){
-       if(err) return res.status(409).send("DB ERROR");
+      if(err) return res.status(500).send("DB ERROR");
+      if(result) return res.status(412).send("already exists");
+      else{
+        var current = new Mydics({
+          owner: token,
+          dicname: dicname,
+          sub: sub,
+          favorite: []
+        });
 
-       if(result.favorite !== undefined && result.favorite !== null){
-         Words.findOne({word: word}, function(err, words){
-            if(err) return res.status(409).send("Db Error");
+        current.save(function(err, data) {
+          if (err) return res.status(500).send("DB error");
+            Mydics.find({owner: token}, function(err, mydic){
+               if(err) res.send(500, "dberror");
+               res.status(200).send(mydic);
+            });
+        });
+      }
+    });
+  })
+
+  .put('/add', function(req, res, next){
+    var params = ['dicname', 'token', 'word'];
+    func.check_param(req.body, params, res);
+    var dicname = req.body.dicname;
+    var token = req.body.token;
+    var word = req.body.word;
+    var check = 0;
+
+    Mydics.findOne({owner: token, dicname: dicname}, function(err, result){
+      if(err) return res.status(500).send("DB ERROR");
+
+      if(result.favorite !== undefined && result.favorite !== null){
+        Words.findOne({word: word}, function(err, words){
+          if(err) return res.status(500).send("Db Error");
              
-            for(var i = 0; i<result.favorite.length; i++){
-              if(result.favorite[i] === words.id){
+          for(var i = 0; i<result.favorite.length; i++){
+            if(result.favorite[i] === words.id){
                 check = 1;
                 break;
-              }
             }
+          }
 
-            if(!check){
-             Mydics.update({owner: token, dicname: dicname}, {$push: {favorite: words.id}}, function(err, com){
-                if(err) return  res.status(409).send("already exists");
+          if(!check){
+            Mydics.update({owner: token, dicname: dicname}, {$push: {favorite: words.id}}, function(err, com){
+              if(err) return  res.status(500).send("already exists");
 
-                Mydics.findOne({owner: token, dicname: dicname}, function(err, mydic){
-                  if(err) res.status(409).send("DB Error");
-                  return res.status(200).send(mydic);
-                });
+              Mydics.findOne({owner: token, dicname: dicname}, function(err, mydic){
+                if(err) res.status(500).send("DB Error");
+                return res.status(200).send(mydic);
+              });
             });
-	   }else{
-              return res.status(409).send("error");
-           }
+	   }else return res.status(500).send("error");
          });
 
        }else{
@@ -75,65 +69,66 @@ router.post('/add', function(req, res, next){
             Mydics.update({owner: token, dicname: dicname}, {$push: {favorite: words.id}}, function(err, com){
                if(err) err;
                Mydics.findOne({owner: token, dicname: dicname}, function(err, mydic){
-                  if(err) return res.status(409).send("DB Error");
+                  if(err) return res.status(500).send("DB Error");
                   res.status(200).send(mydic);
                });
           });
         });
-      }
+       }
     });
-});
+  })
 
-router.post('/pop', function(req, res, next){
+  .put('/pop', (req, res, next) =>{
+    var params = ['dicname', 'token', 'id'];
+    func.check_param(req.body, params, res);
+
     var token = req.body.token;
     var dicname = req.body.dicname;
     var id = req.body.id;
 
     Mydics.update({owner: token, dicname: dicname}, {$pop: {favorite: id}}, function(err, result){
        if(err) err;
-        Mydics.findOne({owner: token, dicname: dicname}, function(err, mydic){
-            if(err) res.status(409).send("DB Error");
-            res.status(200).send(mydic);
+       Mydics.findOne({owner: token, dicname: dicname}, function(err, mydic){
+         if(err) res.status(500).send("DB Error");
+         res.status(200).send(mydic);
        });
     });
-});
+  })
+  
+  .get('/mydic/:token', (req, res) =>{
+    var params = ['token'];
+    func.check_param(req.params, params, res);
+    var token = req.params.token;
 
-router.post('/', function(req, res){
-   var token = req.body.token;
+    Mydics.find({owner: token}, function(err, mydic){
+      if(mydic) res.status(200).send(mydic);
+      else return res.status(500).send("No dic");
+    });
+  })
 
-   Mydics.find({owner: token}, function(err, result){
-       if(result !== null){
-         return res.status(200).send(result);
-       }else{
-         return res.status(409).send("No dic");
-       }
-   });
-});
+  .get('/mydic/:token/:dicname', function(req, res){
+    var params = ['token', 'dicname'];
+    func.check_param(req.params, params, res);
+    var token = req.params.token;
+    var dicname = req.params.dicname;
+    var data = [];
 
-router.post('/detail', function(req, res){
-   var token = req.body.token;
-   var dicname = req.body.dicname;
-   var data = [];
-
-   Mydics.findOne({owner: token, dicname: dicname}, function(err, result){
-       if(result !== null){
-
-         Words.find({}, function(err, word){
-           for(var i = 0; i<result.favorite.length; i++){
-             for(var j = 0; j<word.length; j++){
-               if(result.favorite[i] === word[j].id){
-                 data.push(word[j]);
-                 break;
-               }
+    Mydics.findOne({owner: token, dicname: dicname}, function(err, mydic){
+      if(mydic){
+        Words.find({}, function(err, word){
+          for(var i = 0; i<mydic.favorite.length; i++){
+            for(var j = 0; j<word.length; j++){
+              if(mydic.favorite[i] === word[j].id){
+                data.push(word[j]);
+                break;
               }
             }
+          }
+          return res.status(200).json({dicname: mydic.dicname, subtxt: mydic.sub, wordlist: data});
+        });
+      }else return res.status(404).send("No dic");
+    });
+  });
 
-            return res.status(200).json({dicname: result.dicname, subtxt: result.sub, wordlist: data});
-         });
-       }else{
-         return res.status(409).send("No dic");
-       }
-   });
-});
-
-module.exports = router;
+  return router;
+}
