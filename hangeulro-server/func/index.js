d1fabd94c3826c91b257f0cfbf1df3ee40222e35
iwin2471 <iwin12372@gmail.com>
2017-02-02 00:43:12 +0900

    [modify] func/index check_param

--- hangeulro-server/func/index.js
+++ hangeulro-server/func/index.js
@@ -95,7 +95,7 @@ var upload = (req, res, boardid, date, params) => {
 };
 
 var check_param = (req_param, params) =>{
-  return params.every(str => req_param[str] != undefined || req_param[str] != null);
+  return params.every(str => req_param[str] != undefined || req_param[str] != null || req_param[str] != "");
 }
 
 
